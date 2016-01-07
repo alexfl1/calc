2da36dddc391151089e49573002b59a94f15127b
Alex <alexfl7@ya.ru>
2016-01-07 03:41:22 +0300

    test netaddr

--- t1/q1.py
+++ t1/q1.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+import sys
+import re
+from netaddr import *
 
+#print (sys.argv[1])
+# https://pythonhosted.org/netaddr/tutorial_01.html
 
-print("test")
+IP = sys.argv[1]
+
+i = IPAddress(IP)
+
+print (i.bin)
